8200ccbe7104bbc60b488cd237649e95c38ea089
Rasmus Helleberg Eriksen <rasm239g@hotmail.com>
2024-11-07 12:00:51 +0100

    not done jo

--- deployments/deploy_grafana.bash
+++ deployments/deploy_grafana.bash
@@ -7,4 +7,4 @@ kubectl apply -f grafana.yaml
 
 # Execute commands directly in a new terminal window
 # Use "apt-get install konsole" to install konsole if not already.
-konsole -e sh -c 'echo "Access Grafana with: http://127.0.0.1:3000"; echo "Waiting for pod Ready (timeout=20s) . . ."; kubectl wait --for=condition=Ready pod -l app=grafana --timeout=20s; echo "Start port-forward"; kubectl port-forward svc/grafana 3000'
+konsole -e sh -c 'echo "Access Grafana with: http://127.0.0.1:3000"; echo "Waiting for pod Ready (timeout=20s) . . ."; kubectl wait --for=condition=Ready pod -l app=grafana --timeout=20s; echo "Start port-forward"; kubectl port-forward service/grafana 3000'
